353eca37b603ad4a85ef093aa5229174e021a403
riko <g_anz@mail.ru>
2020-05-27 01:55:06 +0400

    added recipes list and recip content sections

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,31 +5,36 @@ export default function header() {
   return (
     <div>
       <nav className="navbar navbar-dark bg-primary navbar-expand-md navbar-expand-lg">
-        <a className="navbar-brand">Recieps</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <NavLink activeClassName="active" to="/" exact >
-                <span class="nav-link">Home</span>
-              </NavLink>
-            </li>
-            <li class="nav-item">
-              <NavLink activeClassName="active" to="recipes">
-                <span class="nav-link">Recieps</span>
-              </NavLink>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
+        <div className="container">
+          <a className="navbar-brand">Recieps Site</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <NavLink activeClassName="active" to="/" exact>
+                  <span className="nav-link">Home</span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink activeClassName="active" to="recipes">
+                  <span className="nav-link">Recieps</span>
+                </NavLink>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
         </div>
       </nav>
     </div>
--- src/pages/recipes/recipes.js
+++ src/pages/recipes/recipes.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { recipes as recipesData } from "../../static/recipes";
+import "./recipes.scss";
 
-export default function recipes() {
-  return <div className="page-recieps">
-  
-  </div>;
+export default function Recipes() {
+  const [recipt, setRecipt] = useState({});
+
+  return (
+    <div className="page-recieps">
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col col-md-3">
+            <div className="recieps-sidebar">
+              <div>
+                <ul className="list-unstyled text-left">
+                  {recipesData.map((item, i) => (
+                    <li
+                      className={
+                        item.name === recipt.name
+                          ? "recipt-item bg-primary text-white shadow"
+                          : "recipt-item"
+                      }
+                      key={i}
+                      onClick={() => setRecipt(item)}
+                    >
+                      <span className="p-2">{item.name}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col col-md-9">
+            <h5>{recipt.name}</h5>
+            <p>{recipt.description}</p>
+            <span>{recipt.author}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
