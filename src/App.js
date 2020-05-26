e20cd718b148c15958aaeac0e8294f296aadb467
riko <g_anz@mail.ru>
2020-05-26 23:23:41 +0400

    created file structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-const recipes = [
-  {
-    author: "Jim",
-    name: "Chicken Curry",
-    description: "Delicious spicy chicken curry"
-  },
-  {
-    author: "Aravind",
-    name: "Hamburger",
-    description: "Juicy burger with toppings and a soft bun",
-  }
-]
 
 function App() {
   return (
---
+++ src/static/recipes.js
@@ -0,0 +1,14 @@
+const recipes = [
+  {
+    author: "Jim",
+    name: "Chicken Curry",
+    description: "Delicious spicy chicken curry",
+  },
+  {
+    author: "Aravind",
+    name: "Hamburger",
+    description: "Juicy burger with toppings and a soft bun",
+  },
+];
+
+export { recipes };
